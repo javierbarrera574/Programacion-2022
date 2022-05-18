e58a06059a9c75c77ae2c177516efc0fd3ce78c5
javierbarrera574 <82546878+javierbarrera574@users.noreply.github.com>
2022-05-17 23:43:35 -0300

    Add files via upload

---
+++ MesasageBox/MesasageBox/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace MesasageBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.T = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btSalir = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.T.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(62, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(503, 459);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 24);
+            this.label2.TabIndex = 1;
+            // 
+            // T
+            // 
+            this.T.Controls.Add(this.radioButton6);
+            this.T.Controls.Add(this.radioButton5);
+            this.T.Controls.Add(this.radioButton4);
+            this.T.Controls.Add(this.radioButton3);
+            this.T.Controls.Add(this.radioButton2);
+            this.T.Controls.Add(this.radioButton1);
+            this.T.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.T.Location = new System.Drawing.Point(110, 121);
+            this.T.Name = "T";
+            this.T.Size = new System.Drawing.Size(223, 259);
+            this.T.TabIndex = 2;
+            this.T.TabStop = false;
+            this.T.Text = "Tipo de Boton";
+            this.T.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Checked = true;
+            this.radioButton6.Location = new System.Drawing.Point(48, 205);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(111, 28);
+            this.radioButton6.TabIndex = 9;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Cancelar";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.tipoDeBoton_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(48, 171);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(123, 28);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.Text = "Reintentar";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.tipoDeBoton_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(48, 133);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(178, 28);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.Text = "Si/NO. Cancelar";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.tipoDeBoton_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(48, 97);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(79, 28);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Si/No";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.tipoDeBoton_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(48, 64);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(149, 28);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Ok. Cancelar";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.tipoDeBoton_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(48, 31);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 28);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Ok";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.tipoDeBoton_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton7);
+            this.groupBox2.Controls.Add(this.radioButton8);
+            this.groupBox2.Controls.Add(this.radioButton9);
+            this.groupBox2.Controls.Add(this.radioButton10);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(423, 121);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(216, 259);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tipo de Icono";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(55, 64);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(137, 28);
+            this.radioButton7.TabIndex = 8;
+            this.radioButton7.Text = "Información";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.tipoDeIcono_CheckedChanged);
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Cursor = System.Windows.Forms.Cursors.Default;
+            this.radioButton8.Location = new System.Drawing.Point(55, 93);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(139, 28);
+            this.radioButton8.TabIndex = 7;
+            this.radioButton8.Text = "Advertencia";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            this.radioButton8.CheckedChanged += new System.EventHandler(this.tipoDeIcono_CheckedChanged);
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(55, 133);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(151, 28);
+            this.radioButton9.TabIndex = 6;
+            this.radioButton9.Text = "Interrogación";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            this.radioButton9.CheckedChanged += new System.EventHandler(this.tipoDeIcono_CheckedChanged);
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Checked = true;
+            this.radioButton10.Location = new System.Drawing.Point(55, 171);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(75, 28);
+            this.radioButton10.TabIndex = 5;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "Error";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            this.radioButton10.CheckedChanged += new System.EventHandler(this.tipoDeIcono_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(494, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Visualizar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btSalir
+            // 
+            this.btSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSalir.Location = new System.Drawing.Point(366, 404);
+            this.btSalir.Name = "btSalir";
+            this.btSalir.Size = new System.Drawing.Size(88, 33);
+            this.btSalir.TabIndex = 5;
+            this.btSalir.Text = "Salir";
+            this.btSalir.UseVisualStyleBackColor = true;
+            this.btSalir.Click += new System.EventHandler(this.btSalir_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(210, 404);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 33);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 523);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btSalir);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.T);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.T.ResumeLayout(false);
+            this.T.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox T;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btSalir;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MesasageBox/MesasageBox/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MesasageBox
+{
+    public partial class Form1 : Form
+    {
+
+        //Creo la variable privada para usarla en el metodo, y controlar las acciones del típo de boton
+
+
+        private MessageBoxButtons tipoDeBoton = MessageBoxButtons.OK;
+
+        //creo esta variable privada para controlar las acciones del tipo de icono
+
+        private MessageBoxIcon tipoDeIcono = MessageBoxIcon.Error;
+        public Form1()
+        {
+
+
+            InitializeComponent();
+
+
+
+        }
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //variables de cuadro de dialogo
+
+            DialogResult result =
+            MessageBox.Show("Mensaje a desplegar",
+                            "Título de la Ventana",
+
+
+               tipoDeBoton,
+               tipoDeIcono);
+
+            switch (result)
+            {
+                case DialogResult.OK: label2.Text = "Seleccionó OK."; break;
+                case DialogResult.Cancel: label2.Text = "Seleccionó Cancelar."; break;
+                case DialogResult.Yes: label2.Text = "Seleccionó SI."; break;
+                case DialogResult.No: label2.Text = "Seleccionó NO."; break;
+                case DialogResult.Ignore: label2.Text = "Seleccionó Ignorar."; break;
+                case DialogResult.Abort: label2.Text = "Seleccionó Abortar."; break;
+                case DialogResult.Retry: label2.Text = "Seleccionó Reintentar."; break;
+            }
+
+
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+
+        //Creo el metodo para cambiar los valores de los botones de radio
+        //se cargaron todas las referencias donde llamamos al metodo, aca y en el metodo del icono
+        private void tipoDeBoton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sender == radioButton1)
+                tipoDeBoton = MessageBoxButtons.OK;
+            else if (sender == radioButton2)
+                tipoDeBoton = MessageBoxButtons.OKCancel;
+            else if (sender == radioButton3)
+                tipoDeBoton = MessageBoxButtons.YesNo;
+            else if (sender == radioButton4)
+                tipoDeBoton = MessageBoxButtons.YesNoCancel;
+            else if (sender == radioButton5)
+                tipoDeBoton = MessageBoxButtons.RetryCancel;
+            else
+                tipoDeBoton = MessageBoxButtons.AbortRetryIgnore;
+            //investigar bien todo este codigo de arriba
+
+        }
+
+        //creo el metodo para cambiar los valores del tipo de icono
+
+        private void tipoDeIcono_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sender == radioButton7) //icono de error de visualización
+                tipoDeIcono = MessageBoxIcon.Information;
+            else if (sender == radioButton8)
+                tipoDeIcono = MessageBoxIcon.Exclamation;
+            else if (sender == radioButton9)
+                tipoDeIcono = MessageBoxIcon.Question;
+            else //solo queda una opción: mostrar el signo de interrogación
+                tipoDeIcono = MessageBoxIcon.Error;
+
+            //investigar bien este codigo de arriba
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = null;
+        }
+    }
+}
+
+
+
+/*
+ * LA CAJA DE MENSAJES(messagebox), ES UN METODO POLIFORMO
+ * 
+ * YA QUE TIENE VEINTEUN (21) FORMAS DIFERENTES PARA LLARARLO(INVOCARLO. 
+ * 
+ * GroupBox(caja de agrupamiento de elementos)--> ES UNA COLECCION
+ * 
+ * 
+ * LOS RadioButton(BOTONES DE RADIO O DE OPCION), NOS PERMITE SELECCIONAR
+ * 
+ * UNA OPCION DE UNO O MAS COLECCIONES
+ * 
+ * 
+ * PARA HACER QUE FUNCIONE TENGO QUE PROGRAMAR CADA EVENTO DE LOS BOTONES DE RADIO
+ * 
+ * Radiobutton
+ * 
+ * Grupos(GroupBox)
+ * 
+ * Checked
+ * 
+ * 
+ * 
+ * En el código anterior se investiga cual de los radioButtons (mediante sender) cambió su
+estatus de desactivado a activado y se pone la variable tipoDeBoton con uno de los 6 posibles
+valores que tiene la enumeración MessageBoxButtons.
+
+
+Como en el primer método, se investiga cual de los radioButtons (mediante sender) cambió su
+estatus de desactivado a activado y se pone la variable tipoDeIcono con uno de los cuatro
+posibles valores que tiene la enumeración MessageBoxIcon.
+ */
